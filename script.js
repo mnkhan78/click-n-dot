f5d1c6a7ca2ca4d5380bf170fad67343e6cafabd
mnkhan78 <naseem.khan9500078@gmail.com>
2025-05-02 09:13:19 +0530

    Update script.js
    keyboard shortcut added

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ let rmvArr = [];
 document.addEventListener('click', (e) => {
     xcord.innerHTML = `x-cord: ${e.clientX}` //for finding x co-ordinate
     ycord.innerHTML = `y-cord: ${e.clientY}` //for finding y co-ordinate
-    
-    if (e.target.tagName != 'BUTTON'){ //no dot on when we click on button
-        
+
+    if (e.target.tagName != 'BUTTON') { //no dot on when we click on button
+
         const circ = document.createElement('div')
 
         circ.style.height = '20px'
@@ -21,22 +21,22 @@ document.addEventListener('click', (e) => {
         circ.style.left = `${e.clientX}px`
         circ.style.top = `${e.clientY}px`
         circ.style.transform = 'translate(-50%, -50%)'
-    
+
         document.body.appendChild(circ)
-    
+
         dotArr.push(circ);
     }
 
 })
 
 //when we click on remove button
-document.getElementById('rmv-btn').addEventListener ('click', function (e) {
+document.getElementById('rmv-btn').addEventListener('click', function (e) {
 
-    if (dotArr.length != 0){
+    if (dotArr.length != 0) {
         let lastEle = dotArr.pop()
 
         rmvArr.push(lastEle);
-        
+
         document.body.removeChild(lastEle) //to make it disappear
     } else {
         alert('No dot is present on Screen')
@@ -45,14 +45,27 @@ document.getElementById('rmv-btn').addEventListener ('click', function (e) {
 })
 
 //when we click on undo button
-document.getElementById('undo-btn').addEventListener('click', (e) => {
-    if (rmvArr.length != 0){
+
+//undo function
+function undoFunc() {
+    if (rmvArr.length != 0) {
         let lastEle = rmvArr.pop();
 
         dotArr.push(lastEle)
 
         document.body.appendChild(lastEle) //to make it re-appear
     } else {
-        alert ('there is nothing to undo')
+        alert('there is nothing to undo')
+    }
+}
+
+//undo from button
+document.getElementById('undo-btn').addEventListener('click', undoFunc)
+
+//keyboard shortcut for undo
+document.addEventListener('keydown', (e) => {
+    if (e.ctrlKey && e.key.toLocaleLowerCase() === 'z') {
+        e.preventDefault()
+        undoFunc()
     }
 })
